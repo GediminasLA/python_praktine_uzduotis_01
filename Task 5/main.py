c0d0fb0f239f0f0b540b75a50f4548b568dc3632
Gediminas_LA <gediminas.latvenas@gmail.com>
2022-12-07 20:09:43 +0200

    add: task 5 - done

--- Task 5/main.py
+++ Task 5/main.py
@@ -1,5 +1,10 @@
 # Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
 
+from modules.math.composition import composition as addition
+from modules.math.division import division as divivsion
+from modules.math.subtraction import substraction as substraction
+from modules.math.multiplication import multiplication
+from modules.numbers.numbers import one, two, three, four, five
 
 # Kitų failų ir žemiau esančio kodo nekeiskite
 a = addition(one, four);
